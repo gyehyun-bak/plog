c248515d0cf684b29c82c51e88b66904586ce8e2
박계현 <167384362+gyehyun-bak@users.noreply.github.com>
2025-10-09 00:41:54 +0900

    [FEATURE #3]: User 도메인 추가 (#9)
    * feature: User 엔티티 생성
    
    * feature: User에서 isRegisterd 제거

---
+++ server/src/main/java/com/example/server/domain/user/User.java
@@ -0,0 +1,15 @@
+package com.example.server.domain.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer id;
+    @Column(nullable = false, unique = true)
+    private String username;
+}
