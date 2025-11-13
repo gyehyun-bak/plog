ae525912578e47e6edfe6e69fe7b6528c29be461
Gyehyun Park <gyehyunbak@gmail.com>
2025-11-13 22:02:41 +0900

    feature: Post 생성 구현

--- server/src/main/java/com/example/server/domain/post/entity/Post.java
+++ server/src/main/java/com/example/server/domain/post/entity/Post.java
@@ -1,11 +1,33 @@
 package com.example.server.domain.post.entity;
 
+import com.example.server.domain.user.entity.User;
+import com.example.server.global.audit.BaseEntity;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-public class Post {
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Post extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "post_id")
     private Integer postId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    private String title;
+    private String content;
+
+    public static Post create(User author, String title, String content) {
+        return Post.builder()
+                .author(author)
+                .title(title)
+                .content(content)
+                .build();
+    }
 }
--- server/src/main/java/com/example/server/domain/user/entity/User.java
+++ server/src/main/java/com/example/server/domain/user/entity/User.java
@@ -33,6 +33,17 @@ public class User extends BaseEntity {
                 .build();
     }
 
+    /**
+     * 테스트용 User 객체를 생성합니다. 해당 메서드를 실제 비즈니스 코드에서 사용해서는 안 됩니다.
+     * @param userId 테스트용 userId
+     * @return userId를 id로 갖는 빈 User
+     */
+    public static User withId(int userId) {
+        return User.builder()
+                .id(userId)
+                .build();
+    }
+
     public void changeUsername(String username) {
         this.username = username;
     }
