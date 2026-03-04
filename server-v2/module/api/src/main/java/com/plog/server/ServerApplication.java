a62e7d4995d4a7cdf4f3e54778e57611e09755bf
Gyehyun Park <gyehyunbak@gmail.com>
2026-03-04 20:52:35 +0900

    chore: 제어자 public'은(는) Java 25의 'main' 메서드에는 불필요합니다

--- server-v2/module/api/src/main/java/com/plog/server/ServerApplication.java
+++ server-v2/module/api/src/main/java/com/plog/server/ServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = "com.plog")
 public class ServerApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
