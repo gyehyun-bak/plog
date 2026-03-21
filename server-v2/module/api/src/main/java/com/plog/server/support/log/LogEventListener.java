0d3a02c299ac978d69d9d5f00ea18cdd890071dc
Gyehyun Park <gyehyunbak@gmail.com>
2026-03-21 14:57:12 +0900

    refactor: logEventHandler의 로깅 자체 예외 발생 대비 try-catch 추가

--- server-v2/module/api/src/main/java/com/plog/server/support/log/LogEventListener.java
+++ server-v2/module/api/src/main/java/com/plog/server/support/log/LogEventListener.java
@@ -1,6 +1,7 @@
 package com.plog.server.support.log;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -8,13 +9,18 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class LogEventListener {
     private final List<LogEventHandler> handlers;
 
     @EventListener
     public void onLogEvent(LogEvent logEvent) {
         for (LogEventHandler handler : handlers) {
-            handler.handleLogEvent(logEvent);
+            try {
+                handler.handleLogEvent(logEvent);
+            } catch (Exception e) {
+                log.error("Error while handling log event", e);
+            }
         }
     }
 }
